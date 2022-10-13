bd38765e88e28692d2af460bb99164660a072632
Diego Neves <pdcassiano@gmail.com>
2022-10-12 22:15:22 -0300

    feat: add busca de api

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
+import React, { useState } from "react"
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios"
+import Card from './src/components/card'
 
 export default function App() {
+  const [temperatura, setTemperatura] = useState(0)
+  const [cidade, setCidade] = useState('')
+
+
+  const baseURL = "https://api.hgbrasil.com/weather?woeid=455880"
+  let dados = {}
+  const data = axios.get(baseURL)
+  .then(res => {
+      dados = {...res.data}
+      setCidade(dados.results.city_name)
+      setTemperatura(dados.results.temp)
+  })
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Card 
+        cidade = { cidade }
+        temperatura = { temperatura }
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00b4d8',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/components/card.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+
+export default function(props) {
+
+
+    return (
+        <View style={styles.card}>
+            <Text>Tempo agora em {props.cidade}</Text>
+            <Text>{ props.temperatura }°</Text><Text>teste</Text>
+            <Text></Text>
+        </View>
+    )
+
+    
+
+    
+}
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: '#ccc',
+        borderRadius: 20,
+        padding: 15
+    }
+})
+
+
+async function getData() {
+    
+}
